3aed906c0a28313e5ab666ab633de80e434509c8
nelsonlpco <nelson_pc6@hotmail.com>
2015-03-05 13:52:00 -0300

    frm rel hospedes

---
+++ HotelGestor/HotelGestor/frmRelatoriosHospedes.cs
@@ -0,0 +1,34 @@
+﻿using HotelGestor.relatorios.hospedes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HotelGestor
+{
+    public partial class frmRelatoriosHospedes : HotelGestor.template.frmBaseRelatorios
+    {
+        public frmRelatoriosHospedes()
+        {
+            InitializeComponent();
+        }
+
+        private void frmRelatoriosHospedes_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'hotelDBDataSet.CLIENTE' table. You can move, or remove it, as needed.
+            this.cLIENTETableAdapter.Fill(this.hotelDBDataSet.CLIENTE);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            frmView frm = new frmView();
+            frm.setReport();
+            frm.ShowDialog();
+            frm.Dispose();
+        }
+    }
+}

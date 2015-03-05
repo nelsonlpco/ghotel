befabfe3521d1130c6b1770161f7e014d370622c
nelsonlpco <nelson_pc6@hotmail.com>
2015-03-05 14:40:34 -0300

    gerador

--- HotelGestor/HotelGestor/frmRelatoriosHospedes.cs
+++ HotelGestor/HotelGestor/frmRelatoriosHospedes.cs
@@ -18,17 +18,18 @@ namespace HotelGestor
 
         private void frmRelatoriosHospedes_Load(object sender, EventArgs e)
         {
+            // TODO: This line of code loads data into the 'hotelDBDataSet.HOTEL' table. You can move, or remove it, as needed.
+            this.HOTELTableAdapter.Fill(this.hotelDBDataSet.HOTEL);
             // TODO: This line of code loads data into the 'hotelDBDataSet.CLIENTE' table. You can move, or remove it, as needed.
             this.cLIENTETableAdapter.Fill(this.hotelDBDataSet.CLIENTE);
 
+            this.reportViewer1.RefreshReport();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            frmView frm = new frmView();
-            frm.setReport();
-            frm.ShowDialog();
-            frm.Dispose();
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "HotelGestor.relatorios.hospedes.listadehospedes.rdlc";
+            this.reportViewer1.RefreshReport();
         }
     }
 }
